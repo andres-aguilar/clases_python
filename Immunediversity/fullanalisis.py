b6096507648db37583a947345c10611d491801f9
Andres Aguilar <andresyoshimar@gmail.com>
2015-06-30 13:27:44 -0500

    Agregada la funcion dataFrame2list, y agregado notebook de la clase 4

---
+++ Immunediversity/fullanalisis.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#  fullanalisis.py
+#  
+#  Copyright 2015 andresyoshimar@gmail.com
+#  
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#  
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#  
+# 
+
+
+__author__  = 'Andres Aguilar'
+__email__   = 'andresyoshimar@gmail.com'
+__date__   = '24/Jun/2015'
+__version__ = '0.0.1'
+
+"""
+############################################################################################
+# description                                                                              #
+#                                                                                          #
+# How to use:                                                                              #
+############################################################################################
+"""
+
+def saludar(nombre):
+	""" Function: saludar
+
+	Params
+	-------
+	  nombre - nombre de la persona a saludar
+	"""
+	print("hola", nombre)
+
+def despedir(nombre):
+	print("bye", nombre)
